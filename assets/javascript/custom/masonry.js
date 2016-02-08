eebeb7de6d5b8243e07adcf5cd9d6328c485448e
Marcus Cooke <marcus.j.cooke@gmail.com>
2016-02-08 11:32:39 +0000

    Tweak tiles.

--- assets/javascript/custom/masonry.js
+++ assets/javascript/custom/masonry.js
@@ -2,7 +2,7 @@ var $grid = $('.masonry').masonry({
   itemSelector: '.masonry_item',
   percentPosition: true,
   columnWidth: '.masonry_sizer',
-  gutter: 50
+  gutter: 30
 });
 // layout Isotope after each image loads
 $grid.imagesLoaded().progress( function() {
