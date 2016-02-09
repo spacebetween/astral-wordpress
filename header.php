525a2b8928a9e98d202d7a3bb1e8d0de23830e4c
Marcus Cooke <marcus.j.cooke@gmail.com>
2016-02-09 13:42:59 +0000

    Add logo.

--- header.php
+++ header.php
@@ -44,7 +44,7 @@
 		<nav id="site-navigation" class="main-navigation top-bar" role="navigation">
 			<div class="top-bar-title">
 				<ul class="menu">
-					<li class="home"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="main-navigation_item" rel="home"><?php bloginfo( 'name' ); ?></a></li>
+					<li class="home"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="main-navigation_home" rel="home"><img src="http://astral.spacebetween.io/wp-content/uploads/2016/02/astralLogoOrbit.svg" alt="" class="main-navigation_logo"></a></li>
 				</ul>
 			</div>
 			<div class="top-bar-right">
